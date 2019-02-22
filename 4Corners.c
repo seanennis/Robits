e490e908c331c4b4564c6690f56186f47e3828ab
seanennis <seanennis98@gmail.com>
2019-02-22 16:19:04 +0000

    Set motor for arm

--- 4Corners.c
+++ 4Corners.c
@@ -1,5 +1,6 @@
 #pragma config(Sensor, S1,     Touch,          sensorEV3_Touch)//sets up touch
 #pragma config(Motor,  motorB,          motorLeft,     tmotorEV3_Large, PIDControl, encoder)//sets up the left wheel
+#pragma config(Motor,  motorA,          motorArm,    tmotorEV3_Large, PIDControl, encoder)//sets up robot arm
 #pragma config(Motor,  motorC,          motorRight,    tmotorEV3_Large, PIDControl, encoder)//sets up the right wheel
 #pragma config(Sensor, S2, Gyro,     sensorEV3_Gyro)//sets up the gyro
 #pragma config(sensor, S3, Color, sensorEV3_Color, modeEV3Color_Color)//sets up the light sensor to pickup colour
