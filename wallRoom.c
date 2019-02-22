0ace744bb466c224a3b70109c6f7fa82b1abdb09
cinnamonshock <43323944+cinnamonshock@users.noreply.github.com>
2019-02-22 16:57:04 +0000

    Update wallRoom.c

--- wallRoom.c
+++ wallRoom.c
@@ -1,10 +1,9 @@
-#pragma config(Sensor, S1,     Touch,          sensorEV3_Touch)//sets up touch
-#pragma config(Motor,  motorB,          motorLeft,     tmotorEV3_Large, PIDControl, encoder)//sets up the left wheel
-#pragma config(Motor,  motorA,          motorArm,    tmotorEV3_Large, PIDControl, encoder)//sets up robot arm
-#pragma config(Motor,  motorC,          motorRight,    tmotorEV3_Large, PIDControl, encoder)//sets up the right wheel
-#pragma config(Sensor, S2, Gyro,     sensorEV3_Gyro)//sets up the gyro
-#pragma config(sensor, S3, Color, sensorEV3_Color, modeEV3Color_Color)//sets up the light sensor to pickup colour
-#pragma config(Sensor, S4,     Sonar,    sensorEV3_IRSensor, modeEV3IR_Proximity)//sets up the sonar to sense depth
+#pragma config(Sensor, S1,     touch,          sensorEV3_Touch)
+#pragma config(Sensor, S2,     sonor,          sensorEV3_Ultrasonic)
+#pragma config(Sensor, S3,     colour,         sensorEV3_Color, modeEV3Color_Color)
+#pragma config(Motor,  motorA,          A,             tmotorNXT, PIDControl, encoder)
+#pragma config(Motor,  motorB,          B,             tmotorNXT, PIDControl, encoder)
+#pragma config(Motor,  motorC,          C,             tmotorNXT, PIDControl, encoder)
 
 void wallRoom() {
 	//drawBmpfile(0, 127, "US sensor");
