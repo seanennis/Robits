731100761d35a1e7860018fb8b58eeb02471b267
Jsheen27 <44396836+Jsheen27@users.noreply.github.com>
2019-02-22 17:05:37 +0000

    Update rescueRoom.c

--- rescueRoom.c
+++ rescueRoom.c
@@ -1,12 +1,12 @@
-#pragma config(Sensor, S1,     Touch,          sensorEV3_Touch)//sets up touch
+#pragma config(Sensor, S4,     Touch,          sensorEV3_Touch)//sets up touch
 #pragma config(Motor,  motorB,          motorLeft,     tmotorEV3_Large, PIDControl, encoder)//sets up the left wheel
 #pragma config(Motor,  motorA,          motorArm,    tmotorEV3_Large, PIDControl, encoder)//sets up robot arm
 #pragma config(Motor,  motorC,          motorRight,    tmotorEV3_Large, PIDControl, encoder)//sets up the right wheel
-#pragma config(Sensor, S2, Gyro,     sensorEV3_Gyro)//sets up the gyro
+#pragma config(Sensor, S1, Gyro,     sensorEV3_Gyro)//sets up the gyro
 #pragma config(sensor, S3, Color, sensorEV3_Color, modeEV3Color_Color)//sets up the light sensor to pickup colour
-#pragma config(Sensor, S4,     Sonar,    sensorEV3_IRSensor, modeEV3IR_Proximity)//sets up the sonar to sense depth
+#pragma config(Sensor, S2,     Sonar,    sensorEV3_IRSensor, modeEV3IR_Proximity)//sets up the sonar to sense depth
 
-task main() {
+void rescueRoom() {
 	drawBmpfile(0, 127, "Evil");
 
 	while(sensorValue(Sonar) > 8) {
