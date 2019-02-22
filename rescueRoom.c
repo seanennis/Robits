e30729a57a1065b65708f1a4980f986bc9a453db
seanennis <seanennis98@gmail.com>
2019-02-22 16:17:46 +0000

    Update rescueRoom.c

--- rescueRoom.c
+++ rescueRoom.c
@@ -1,5 +1,6 @@
 #pragma config(Sensor, S1,     Touch,          sensorEV3_Touch)//sets up touch
 #pragma config(Motor,  motorB,          motorLeft,     tmotorEV3_Large, PIDControl, encoder)//sets up the left wheel
+#pragma config(Motor,  motorA,          motorArm,    tmotorEV3_Large, PIDControl, encoder)//sets up robot arm
 #pragma config(Motor,  motorC,          motorRight,    tmotorEV3_Large, PIDControl, encoder)//sets up the right wheel
 #pragma config(Sensor, S2, Gyro,     sensorEV3_Gyro)//sets up the gyro
 #pragma config(sensor, S3, Color, sensorEV3_Color, modeEV3Color_Color)//sets up the light sensor to pickup colour
@@ -7,4 +8,11 @@
 
 void rescueRoom() {
 	drawBmpfile(0, 127, "Evil");
+
+	while(sensorValue(Sonar) >= 5) {
+		setMotorSpeed(motorRight, 30);
+		setMotorSpeed(motorLeft, 30);
+	}
+	setMotorSpeed(motorArm, 5);
+	wait1Msec(1000);
 }
