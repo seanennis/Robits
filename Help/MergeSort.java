bc00a9c414cbd8b09fa3b5e12a306599bd0400a6
seanennis <seanennis98@gmail.com>
2019-04-02 18:12:43 +0100

    Update MergeSort.java

--- Help/MergeSort.java
+++ Help/MergeSort.java
@@ -4,7 +4,9 @@ import dsaii.lists.LinkedList;
 import dsaii.lists.List;
 
 import java.lang.reflect.Array;
+import java.util.Comparator;
 import java.util.Random;
+import dsaii.common.Position;
 
 public class MergeSort {
     /**
@@ -17,9 +19,41 @@ public class MergeSort {
         while (list.size() < 100) {
             list.insertLast(random.nextInt(100));
         }
+        
         System.out.println(list);
         mergeSort(list);
         System.out.println(list);
+        
+        Random random2 = new Random();
+        List<Integer> list2 = new LinkedList<Integer>();
+        while (list2.size() < 100) {
+            list2.insertLast(random2.nextInt(100));
+        }
+        
+        Comparator<Integer> C = new Comparator<Integer>() {
+        	public int compare(Integer x, Integer y) {
+        		return x.compareTo(y);
+            }
+        };
+        
+        System.out.println(list2);
+        mergeSort2(list2, C);
+        List<Integer> tempList = new LinkedList<Integer>();
+        tempList = list;
+        Position<Integer> temp;
+        
+        for(int i = 0; i < (list.size()/2); i++) {
+        	if(i == 0) {
+        		temp = list.last();
+        		tempList.insertFirst(temp.element());
+        	}
+        	else {
+        		temp = list.next(temp);
+        		tempList.insertLast(list.remove(temp));
+        	}
+        }
+        list = tempList;
+        System.out.println(list2);
     }
 
     /**
@@ -35,6 +69,16 @@ public class MergeSort {
             merge(S, sublists[0], sublists[1]);
         }
     }
+    
+    public static <T extends Comparator<T>> void mergeSort2(List<T> list, Comparator<T> C) {
+        if (list.size() > 1) {
+            List<T>[] sublists = partition(list, list.size()/2);
+            mergeSort2(sublists[0], C);
+            mergeSort2(sublists[1], C);
+            merge2(list, sublists[0], sublists[1], C);
+        }
+    }
+
 
     /**
      * The partitioning algorithm
@@ -65,5 +109,36 @@ public class MergeSort {
      * @param <T> the type of value stored in the list
      */
     private static <T extends Comparable<T>> void merge(List<T> S, List<T> S1, List<T> S2) {
+    	while(!(S1.isEmpty()) && !(S2.isEmpty())) {
+    		if(S1.first().element().compareTo(S2.first().element()) < 0)
+    			S.insertLast(S1.remove(S1.first()));
+    		else
+    			S.insertLast(S2.remove(S2.first()));
+    	}
+    		
+    	while(!(S1.isEmpty())) {
+    		S.insertLast(S1.remove(S1.first()));
+    	}
+    	
+    	while(!(S2.isEmpty())) {
+    		S.insertLast(S2.remove(S2.first()));
+    	}
+    }
+    
+    private static <T extends Comparator<T>> void merge2(List<T> S, List<T> S1, List<T> S2, Comparator<T> C) {
+    	while(!(S1.isEmpty()) && !(S2.isEmpty())) {
+    		if(C.compare(S1.first().element(), S2.first().element()) >= 0)
+    			S.insertLast(S1.remove(S1.first()));
+    		else
+    			S.insertLast(S2.remove(S2.first()));
+    	}
+    		
+    	while(!(S1.isEmpty())) {
+    		S.insertLast(S1.remove(S1.first()));
+    	}
+    	
+    	while(!(S2.isEmpty())) {
+    		S.insertLast(S2.remove(S2.first()));
+    	}
     }
 }
